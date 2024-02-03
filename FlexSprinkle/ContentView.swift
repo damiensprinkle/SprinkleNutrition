f2b8e5fcf01458f8b9ff5d8d521a0194af4e8538
damien sprinkle <damien.sprinkle@gmail.com>
2024-02-02 18:03:07 -0600

    Initial Commit

---
+++ FlexSprinkle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlexSprinkle
+//
+//  Created by Damien Sprinkle on 2/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlexSprinkle/FlexSprinkleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexSprinkleApp.swift
+//  FlexSprinkle
+//
+//  Created by Damien Sprinkle on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FlexSprinkleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
